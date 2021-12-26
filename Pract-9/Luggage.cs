0ba0fee1aa5ea37cf6a30c08e3dac63665a36471
Кирилл Соколов <sokolov17925@yandex.ru>
2021-12-26 21:06:33 +0300

    Добавьте файлы проекта.

---
+++ Pract-9/Luggage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_9
+{
+    struct Luggage
+    {
+        public Luggage(int numberofthing, double weight)
+        {
+            numberOfThing = numberofthing;
+            Weight = weight;
+        }
+
+        public int numberOfThing { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
